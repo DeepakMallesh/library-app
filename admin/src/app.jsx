01de7e097a8c8249a84e2406df441afc2660d18c
Darshini M S <darshinims00@gmail.com>
2025-10-03 12:58:04 +0530

    ip remove

--- admin/src/app.jsx
+++ admin/src/app.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import AdminPanel from './pages/AdminPanel';
-import IPGuard from './components/IPGuard';
 
 function App() {
   return (
-    <IPGuard>
-      <AdminPanel />
-    </IPGuard>
+    <AdminPanel />
   );
 }
 
